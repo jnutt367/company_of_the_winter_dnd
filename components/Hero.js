6eaf9f9bc93becf5976e41d9ce084160e2a7b017
Jason Nutt <jnutt367@gmail.com>
2021-10-06 16:13:16 -0400

    commit for less read on hero

--- components/Hero.js
+++ components/Hero.js
@@ -15,8 +15,8 @@ const Hero = () => {
         <div className="bg-indigo-900 bg-opacity-40 flex items-center h-full absolute w-full z-10">
           <div className="px-10 max-w-xl my-10">
             <h2 className="bg-indigo-900 bg-opacity-30 font-serif text-2xl text-white font-semibold">The Forgotten Realms</h2>
-            <p className="mt-2 text-white">
-            A dark cloud of uncertainty envelops the land as the swarm of dragons seems to increase & circle like sharks of the sky over the land. <i className="font-mono text-green-100"> The Company, a rag tag group of miscellaneously skilled adventurers </i> , is voluntered out of necessity to get to the bottom of what could be making these Dragons so interested in devouring the town of Dungril. 
+            <p className="bg-indigo-900 bg-opacity-50 mt-2 text-white">
+            A dark cloud of uncertainty envelops the land as the swarm of dragons seems to increase & circle like sharks of the sky over the land. <i className="font-mono text-green-100"> The Company, a rag tag group of miscellaneously skilled adventurers </i> , is voluntered to get to the bottom of what could be making these Dragons interested in devouring Dungril. 
             </p>
             <Link href={`/characters`}>
               <button className="font-serif flex items-center mt-4 px-3 py-2 bg-indigo-600 text-white text-sm uppercase font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
