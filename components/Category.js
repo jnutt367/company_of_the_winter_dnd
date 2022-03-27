8aac02988003c6959cc9580d3e100daf02bde5be
Jason Nutt <jnutt367@gmail.com>
2022-03-27 16:18:02 -0400

    commit for playin with borders man

--- components/Category.js
+++ components/Category.js
@@ -3,12 +3,12 @@ import Image from 'next/dist/client/image'
 const Category = ({ category, categoryCount }) => {
     return (
         <>
-        <div className="bg-black-300 border-8 border-dashed border-zinc-600 rounded-lg">
+        <div className="bg-black border-2 border-dotted border-zinc-600 rounded-lg">
        
-        <h3 className="mb-10 font-sans p-4 text-white text-2xl font-medium block mt-16">
+        <h3 className="mb-10  border-dotted border-zinc-600 bg-red-500 font-sans p-4 text-black text-2xl font-medium block mt-16">
             {category}
         </h3>
-        <span className="font-mono p-8 text-md text-white border-8 border-solid border-zinc-600 rounded-lg">{categoryCount}</span>
+        <span className="font-mono p-8 text-md bg-red-100 text-black border-2 rounded-lg">{categoryCount}</span>
         </div>
         </>
         )
