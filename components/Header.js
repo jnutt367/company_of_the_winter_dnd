b5d8ae85d041a77350e5d1c9bcc994ba9d6e4fe5
Jason Nutt <jnutt367@gmail.com>
2022-03-30 20:25:21 -0400

    commit new title

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           <div className="flex text-green items-center justify-between">
             <Link href="/">
               <div className="font-sans w-full text-white text-2xl font-semibold cursor-pointer">
-                RPG Character Creator & Selector
+                D&D Character Creator
               </div>
             </Link>
             <div className="flex items-center justify-end w-full">
