00f5b98dda3e305e3c781da64f6062008a0ade97
Jason Nutt <jnutt367@gmail.com>
2022-04-02 14:35:05 -0400

    commit for better layout

--- components/Character.js
+++ components/Character.js
@@ -6,16 +6,18 @@ import Category from './Category';
 const Character = ({ character }) => {
     return (
         <Link href={`/characters/${character._id}`}>
-        <div className="mt-0 w-full max-w-sm p-4 mx-auto rounded-md shadow-md overflow-hidden cursor-pointer hover:shadow-2xl transition">
-          <div className="flex items-end justify-end h-screen w-full bg-contain relative">
+        <div className="mt-0 border-8 border-zinc-800 w-full max-w-sm p-4 mx-auto rounded-md shadow-md overflow-hidden cursor-pointer hover:shadow-2xl transition">
+          <div className="flex items-end justify-end h-screen w-full bg-cover relative">
+          <p>Hello there</p>
+          <hr></hr>
             <Image
+              className="z-0 border-8 border-zinc-800 rounded"
               src={character.image}
               alt={character.character_name}
               layout="fixed"
               width="400px"
               height="400px"
               objectFit="fill"
-              className="z-0 border-4 border-zinc-800 rounded"
             />
             <button className="absolute z-10 p-2 rounded-full bg-black text-white mx-5 -mb-4 hover:bg-green-600 focus:outline-none focus:bg-indigo-500">
               <UserIcon className="w-5 h-5" />
